eebb82bafc60539e8e947578f2c4210e715166b7
Rudi Persson <rudi0234@edu.easj.dk>
2017-01-31 11:18:52 +0100

    Opage 2.5 er færdig. Det med at adde eventHandler og createEvent.

---
+++ EventMaker/Handler/EventHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMaker.ViewModel;
+using EventMaker.Model;
+using EventMaker.Converter;
+
+namespace EventMaker.Handler
+{
+    class EventHandler
+    {
+        /*Properties*/
+        public EventViewModel EventViewModel { get; set; }
+
+
+        /*Constroctur*/
+        public EventHandler(EventViewModel evm)
+        {
+            this.EventViewModel = evm;
+        }
+
+        /*Metodes*/
+        public void CreateEvent()
+        {
+            Event nyEvent = new Event(EventViewModel.Id, EventViewModel.Name, EventViewModel.Description, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel._date, EventViewModel._time));
+             
+          
+        }
+    }
+}
--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -10,6 +10,7 @@ namespace EventMaker.ViewModel
     class EventViewModel
     {
 
+        /*Singelton*/
         private EventCatalogSingleton _eventSingleton;
 
         public EventCatalogSingleton Eventsingelton
@@ -18,10 +19,24 @@ namespace EventMaker.ViewModel
             set { _eventSingleton = value; }
         }
 
+        /*Properties*/
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTimeOffset _date { get; set; }
+        public TimeSpan _time { get; set; }
 
+
+        /*Constructor*/
         public EventViewModel()
         {
             Eventsingelton = EventCatalogSingleton.Instance;
+
+            DateTime dt = System.DateTime.Now;
+            _date = new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0, new TimeSpan());
+            _time = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
+
         }
 
     }
